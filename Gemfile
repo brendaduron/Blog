69a6a4d691d85978dd4898447ce154a328c6fc10
brendaduron <brenda.duron@cimat.mx>
2014-03-12 11:05:05 -0600

    Siguiendo el code standar

--- Gemfile
+++ Gemfile
@@ -1,19 +1,20 @@
 # gemfile
 
 source 'https://rubygems.org'
-ruby "2.0.0"
+ruby '2.0.0'
 
-gem "sinatra"
-gem "activerecord"
-gem "sinatra-activerecord"
+gem 'sinatra'
+gem 'activerecord'
+gem 'sinatra-activerecord'
 gem 'sinatra-flash'
 gem 'sinatra-redirect-with-flash'
 gem 'rake'
-gem "sqlite3", :platform => [:ruby, :mswin, :mingw]
+gem 'sqlite3', :platform => [:ruby, :mswin, :mingw]
+gem 'rubocop'
 
 group :development do
   gem 'sqlite3'
-  gem "tux"
+  gem 'tux'
 end
 
 group :production do
--- environments.rb
+++ environments.rb
@@ -7,11 +7,11 @@ configure :production do
   db = URI.parse(ENV['DATABASE_URL'] || 'postgress:///localhost/mydb')
 
   ActiveRecord::Base.establish_connection(
-                                          :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-                                          :host => db.host,
-                                          :username => db.user,
-                                          :password => db.password,
-                                          :database => db.path[1..-1],
-                                          :encodig => 'utf8'
-                                          )
+                                           adapter: db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+                                           host: db.host,
+                                           username: db.user,
+                                           password: db.password,
+                                           database: db.path[1..-1],
+                                           encodig: 'utf8'
+                                         )
 end
