133702f00cf6f16930c5586df0ac54e2037933cf
brendaduron <brenda.duron@cimat.mx>
2014-03-13 10:06:13 -0600

    =)

--- MyTest.rb
+++ MyTest.rb
@@ -20,25 +20,52 @@ class MyTest < MiniTest::Unit::TestCase
   def test_root
     get '/'
     assert last_response.ok?
-    # last_response.status.must_equal 200
-    # assert_equal 'Welcome.', last_response.headers['title']
+    assert_equal 200, last_response.status
   end
 
-  def test_create
+  def test_linkcreate
     get '/posts/create'
     assert last_response.ok?
+    assert_equal 200, last_response.status
+    @post = Post.new
+    assert last_request
+    request 'posts/create'
+    assert last_request
   end
 
- # def test_createSave
-  #  post "/posts" , {:name => 'Prueba', :body => 'Hola'}
-   # assert last_response.ok?
-   # last_response.status.must_equal 200
-   # assert last_response.body.should include("Congrats")
+  def test_createsave_nobody
+    post '/posts' , params = { title: 'Prueba' }
+    @post = Post.new(params)
+    assert !@post.save
+  end
 
- # end
+  def test_createsave_notitle
+    post '/posts' , params = { body: 'Prueba' }
+    @post = Post.new(params)
+    assert !@post.save
+  end
 
- # def test_consultar
- #   get "/posts/:id", "id" => 2
- #   assert last_response.ok?
- # end
+  def test_consultar
+    get '/posts/:id', params = { id: 2 }
+    assert_equal 'http://example.org/posts/:id?id=2', last_request.url
+    assert last_request
+    assert_equal '2', last_request.params['id']
+  end
+
+  def test_consultarerror
+    get '/posts/:id', params = { id: 'A' }
+    assert_equal 'http://example.org/posts/:id?id=A', last_request.url
+    assert last_request
+    get '/posts/:id?id=A'
+    assert_equal 500, last_response.status
+  end
+
+  def test_search
+    get '/search' , params = { query: 'brenda' }
+    assert last_response.ok?
+    @posts = Post.where("title like '%#{params[:query]}%'")
+    assert last_request
+    get '/posts/index'
+    assert_equal 500, last_response.status
+  end
 end
