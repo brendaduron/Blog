e244594abea93478f8d3f4ea3d6c232c11bb4ae0
xduvox <xduvox@gmail.com>
2014-03-13 02:25:17 -0700

    ya mero
    arriba jerez

---
+++ editar.rb
@@ -0,0 +1,72 @@
+require 'json'
+require 'selenium-webdriver'
+gem 'test-unit'
+require 'test/unit'
+
+# el pollito pio
+class Editar < Test::Unit::TestCase
+  def setup
+    @driver = Selenium::WebDriver.for :firefox
+    @base_url = 'http://blogvyv.herokuapp.com'
+    @accept_next_alert = true
+    @driver.manage.timeouts.implicit_wait = 30
+    @verification_errors = []
+  end
+
+  def teardown
+    @driver.quit
+    assert_equal [], @verification_errors
+  end
+
+  def test_editar
+    @driver.get(@base_url + '/')
+    @driver.find_element(:link, 'Nuevo Post').click
+    @driver.find_element(:id, 'post_title').clear
+    @driver.find_element(:id, 'post_title').send_keys 'selenium editar'
+    @driver.find_element(:id, 'post_body').clear
+    @driver.find_element(:id, 'post_body').send_keys 'el pollito PIO'
+    @driver.find_element(:css, 'button.btn.btn-success').click
+    verify { assert_equal 'selenium editar', @driver.find_element(:css, 'h1').text }
+    @driver.find_element(:link, 'Editar Post').click
+    @driver.find_element(:id, 'post_body').clear
+    @driver.find_element(:id, 'post_body').send_keys 'el pollito PIO, el pollito pio pio pio,'
+    @driver.find_element(:id, 'post_title').clear
+    @driver.find_element(:id, 'post_title').send_keys 'selenium editar del pollito Pio'
+    @driver.find_element(:css, 'button.btn.btn-success').click
+    verify { assert_equal 'selenium editar del pollito Pio', @driver.find_element(:css, 'h1').text }
+    @driver.find_element(:link, 'Borrar Post').click
+  end
+
+  def element_present?(how, what)
+    receiver.find_element(how, what)
+    true
+  rescue Selenium::WebDriver::Error::NoSuchElementError
+    false
+  end
+
+  def alert_present?
+    receiver.switch_to.alert
+    true
+  rescue Selenium::WebDriver::Error::NoAlertPresentError
+    false
+  end
+
+  def verify(&blk)
+    yield
+  rescue Test::Unit::AssertionFailedError => ex
+    @verification_errors << ex
+  end
+
+  def close_alert_and_get_its_text(how, what)
+    alert = receiver.switch_to.alert
+    alert_text = alert.text
+    if @accept_next_alert
+      alert.accept
+    else
+      alert.dismiss
+    end
+    alert_text
+  ensure
+    @accept_next_alert = true
+  end
+end
